5d555432a63b84b424c4c7c7f65420def034dcab
Enzo-LO <enzoleitesjp@gmail.com>
2025-09-05 11:35:15 -0300

    up2
    legal

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Olá Mundo!</Text>
+    </View>
+  );
+}
+
+
