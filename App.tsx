2f42aaffed504bf948cec370d3b388d9942f7900
Enzo-LO <enzoleitesjp@gmail.com>
2025-09-26 07:47:49 -0300

    e
    e

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "./src/screens/SignIn";
 
 export  default function App() {
   return (
-   
+    <SignIn/>
   );
 }
 
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, TextInput, Image } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { stylesSignIn } from './styles';
 import IllustrationImg from '../../assets/favicon.png';
 
 export function SignIn() {
   return (
     <View style={stylesSignIn.container}>
+
       <Image source={IllustrationImg}/>
+
     </View>
   );
 }
